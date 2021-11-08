7d18b5198b6947f2e392ad5df81b098b9accb8f2
sonud4 <gn.sonuda@gmail.com>
2021-11-08 14:02:44 +0900

    発言まで

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Intents, Massage } from "discord.js";
 import Discordjs from "discord,js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,7 +12,11 @@ const client = new Discordjs.Client({
     ]
 });
 
-client.on("message", async msg => {
+client.on('ready', () => {
+    console.log(`${client.user.tag} でログインしています。`)
+  })
+
+client.on("messageCreate", async msg => {
     if(msg.content === "!hey"){
         msg.reply("hey!");
     }
